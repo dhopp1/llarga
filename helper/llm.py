5f8ce12eaf75576c8a04eb6796ed4c905c6f34bd
dhopp1 <daniel.hopp1@gmail.com>
2025-04-01 10:26:07 +0200

    Switching to use openai python package instead of requests

--- helper/llm.py
+++ helper/llm.py
@@ -1,21 +1,11 @@
 from datetime import datetime
-import json
 from openai import OpenAI
-import re
-import requests
 import streamlit as st
 
 
 def gen_llm_response(query, messages_input=[]):
     """Create the data required for an LLM call"""
     messages = messages_input
-
-    # llm url
-    llm_url = (
-        st.session_state["llm_info"]
-        .loc[lambda x: x["name"] == st.session_state["selected_llm"], "llm_url"]
-        .values[0]
-    ) + "/chat/completions"
 
     # llm model name
     llm_model_name = (
@@ -24,51 +14,24 @@ def gen_llm_response(query, messages_input=[]):
         .values[0]
     )
 
-    llm_headers = {
-        "Content-Type": "application/json",
-        "Authorization": f'Bearer {st.session_state["llm_api_key"]}',
-    }
+    client = OpenAI(
+        api_key=st.session_state["llm_api_key"],
+        base_url=st.session_state["llm_info"]
+        .loc[lambda x: x["name"] == st.session_state["selected_llm"], "llm_url"]
+        .values[0],
+    )
 
-    llm_data = {
-        "model": llm_model_name,
-        "temperature": st.session_state["temperature"],
-        "max_tokens": st.session_state["max_tokens"],
-        "messages": messages,
-        "stream": True,
-    }
+    response = client.chat.completions.create(
+        model=llm_model_name,
+        temperature=st.session_state["temperature"],
+        max_tokens=st.session_state["max_tokens"],
+        messages=messages,
+        stream=True,
+    )
 
-    if "gemini" in llm_model_name:
-        client = OpenAI(
-            api_key=st.session_state["llm_api_key"],
-            base_url=st.session_state["llm_info"]
-            .loc[lambda x: x["name"] == st.session_state["selected_llm"], "llm_url"]
-            .values[0],
-        )
-        response = client.chat.completions.create(
-            model=llm_model_name,
-            messages=messages,
-            stream=True,
-        )
-        for chunk in response:
+    for chunk in response:
+        if chunk.choices[0].delta.content is not None:
             yield chunk.choices[0].delta.content
-    else:
-        response = requests.post(
-            llm_url, headers=llm_headers, data=json.dumps(llm_data), stream=True
-        )
-
-        for chunk in response.iter_content(chunk_size=1024, decode_unicode=True):
-            if st.session_state["stop_generation"]:
-                st.session_state["generating"] = False
-                break
-            try:
-                tokens = [
-                    json.loads(_)["delta"]["content"]
-                    for _ in re.findall(r"\[([^\]]*)\]", chunk)
-                ]
-                for token in tokens:
-                    yield token
-            except:
-                pass
 
     yield f"""<br> <sub><sup>{datetime.now().strftime("%Y-%m-%d %H:%M")}</sup></sub>"""
 
